a08ae22bb1c7ac4968706380c439663cddf340f8
Marco Randazzo <marco.randazzo@iit.it>
2021-05-21 10:47:35 +0200

    big changes

--- src/libraries/icubmod/bcbBattery/bcbBattery.cpp
+++ src/libraries/icubmod/bcbBattery/bcbBattery.cpp
@@ -13,14 +13,13 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <cmath>
 
 using namespace std;
 //#define DEBUG_TEST 1
 
 bool BcbBattery::open(yarp::os::Searchable& config)
 {
-    bool correct=true;
-
     //debug
     yDebug("%s\n", config.toString().c_str());
 
@@ -39,7 +38,7 @@ bool BcbBattery::open(yarp::os::Searchable& config)
         return false;
     }
 
-    int period=config.find("thread_period").asInt();
+    int period=group_general.find("thread_period").asInt();
     setPeriod((double)period/1000.0);
 
     Property prop;
@@ -70,7 +69,7 @@ bool BcbBattery::open(yarp::os::Searchable& config)
     // Other options
     this->verboseEnable = group_general.check("verbose", Value(0), "enable/disable the verbose mode").asBool();
     this->screenEnable = group_general.check("screen", Value(0), "enable/disable the screen output").asBool();
-    this->debugEnable = group_general.check("debug", Value(0), "enable/disable the debug mode").asBool();
+    this->silenceSyncWarnings = group_general.check("silence_sync_warnings", Value(0), "enable/disable the print of warnings in case of sync errors.").asBool();
 
     PeriodicThread::start();
     return true;
@@ -79,7 +78,14 @@ bool BcbBattery::open(yarp::os::Searchable& config)
 bool BcbBattery::close()
 {
     //stop the thread
-    PeriodicThread::stop();
+    PeriodicThread::askToStop();
+
+    if (pSerial)
+    {
+        char c = 0x00;
+        bool ret = pSerial->send(&c, 1);
+        if (ret == false) { yError("BcbBattery problems while stopping the transmission");}
+    }
 
     //stop the driver
     driver.close();
@@ -96,12 +102,21 @@ bool BcbBattery::threadInit()
     battery_temperature = 0.0;
     timeStamp = yarp::os::Time::now();
 
-    //start the transmission
-    char c = 0x01;
     if (pSerial)
     {
-        bool ret = pSerial->send(&c, 1);
+        //start the transmission
+        char cmd = 0x01;
+        bool ret = pSerial->send(&cmd, 1);
         if (ret == false) { yError("BcbBattery problems starting the transmission"); return false; }
+
+        //empty the buffer
+        char c = 0;
+        int r = 0;
+        do
+        {
+            r = pSerial->receiveChar(c);
+        } while (r != 0);
+        yInfo("BcbBattery started succesfully");
     }
     else
     {
@@ -125,23 +140,21 @@ void BcbBattery::run()
     }
 
     //read battery data.
-    //if nothing is received, rec=0, the while exits immediately. The string will be not valid, so the parser will skip it and it will leave unchanged the battery status (voltage/current/charge)
-    //if a text line is received, then try to receive more text to empty the buffer. If nothing else is received, serial_buff will be left unchanged from the previous value. The loop will exit and the sting will be parsed.
-    serial_buff[0] = 0;
-
     if (pSerial)
     {
         //empty the buffer
+        serial_buff[0] = 0;
         char c = 0;
         int r = 0;
+        //extracts characters until nothing else is received
         do
         {
            r = pSerial->receiveChar(c);
         } while (r != 0);
 
     search_0:
-        pSerial->receiveChar(serial_buff[8]); if (serial_buff[8] != '\r') { yWarning("BcbBattery sync error r"); goto search_0; }
-        pSerial->receiveChar(serial_buff[9]); if (serial_buff[9] != '\n') { yWarning("BcbBattery sync error n"); goto search_0; }
+        pSerial->receiveChar(serial_buff[8]); if (serial_buff[8] != '\r') { if (!silenceSyncWarnings) yWarning("BcbBattery sync error r"); goto search_0; }
+        pSerial->receiveChar(serial_buff[9]); if (serial_buff[9] != '\n') { if (!silenceSyncWarnings) yWarning("BcbBattery sync error n"); goto search_0; }
         pSerial->receiveChar(serial_buff[0]); if (serial_buff[0] != '\0') {goto search_0; }
         pSerial->receiveChar(serial_buff[1]); //voltage
         pSerial->receiveChar(serial_buff[2]); //voltage
@@ -163,13 +176,34 @@ void BcbBattery::run()
     battery_current = 5.0;
     battery_charge = 72.0;
     battery_temperature = 35.0;
-#endif
+    backpack_status =0;
+#else
 
-    if (verboseEnable) yDebug("BcbBattery::run() serial_buffer is: %s", serial_buff);
-    battery_voltage = (unsigned char) serial_buff[1] * 256 + (unsigned char) serial_buff[2];
-    battery_current = (unsigned char) serial_buff[3] * 256 + (unsigned char) serial_buff[4];
-    battery_charge =  (unsigned char) serial_buff[5] * 256 + (unsigned char) serial_buff[6];
+    if (verboseEnable)
+    {
+        char hexBuffer[31];
+        char decBuffer[41];
+        for (size_t i = 0; i < 10; ++i)
+        {
+            if (output[i])
+            {
+                sprintf(hexBuffer + 3*i,"%02X ", (unsigned int)(serial_buff[i] & 0xFF));
+                sprintf(decBuffer + 4*i,"%03u ", (unsigned int)(serial_buff[i] & 0xFF));
+            }
+            else
+            {
+                sprintf(hexBuffer + 3*i,"-- ");
+                sprintf(decBuffer + 4*i,"--- ");
+            }
+        }
+
+        yDebug("BcbBattery::run() serial_buffer is: (hex) %s, (dec) %s", hexBuffer, decBuffer);
+    }
+    battery_voltage = ((unsigned char) serial_buff[1] * 256 + (unsigned char) serial_buff[2])/1000.0;
+    battery_current = ((unsigned char) serial_buff[3] * 256 + (unsigned char) serial_buff[4])/1000.0;
+    battery_charge =  ((unsigned char) serial_buff[5] * 256 + (unsigned char) serial_buff[6]);
     backpack_status = (unsigned char) serial_buff[7];
+#endif
 
     //add checksum verification
     //...
@@ -211,13 +245,15 @@ bool BcbBattery::getBatteryCharge(double &charge)
 
 bool BcbBattery::getBatteryStatus(Battery_status &status)
 {
-    //yError("Not yet implemented");
-    return false;
+    //The BCB battery indicator does not provide this info, so we simply return BATTERY_OK_IN_USE
+    status=Battery_status::BATTERY_OK_IN_USE;
+    return true;
 }
 
 bool BcbBattery::getBatteryTemperature(double &temperature)
 {
     //yError("Not yet implemented");
+    temperature = std::nan("");
     return false;
 }
 
